713fb5c5f8e30afd96ff43483b6bb01cb0c47f60
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-26 20:10:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const byteSize = (str) => {
   // write your code here
-};
-
+	
+	return(  new Blob(["안녕하세요"]).size);
+}; 
 // Do not change the code below
-const str = prompt("Enter some string.");
+const str = prompt("Enter some string."); 
 alert(byteSize(str));
